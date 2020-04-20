7c05a4b71f356003229066af75e759a60e98162f
cyrilf <cyrilf7x@gmail.com>
2020-04-20 15:41:51 +0200

    Add transition/hover/animation on play/stop button
    close #3

--- components/PlayToggle.vue
+++ components/PlayToggle.vue
@@ -1,42 +1,10 @@
 <template>
   <button
-    class="cursor-pointer absolute"
+    class="absolute"
     :class="{ 'play-button': !isPlaying, 'stop-button': isPlaying }"
     :aria-label="isPlaying ? 'Stop' : 'Start'"
     :title="isPlaying ? 'Stop' : 'Start'"
-  >
-    <svg
-      v-if="!isPlaying"
-      viewBox="0 0 512 512"
-      xmlns="http://www.w3.org/2000/svg"
-      width="75"
-      class="fill-current text-blue-300 hover:text-blue-200"
-    >
-      <polygon points="">
-        <animate
-          attributeType="XML"
-          attributeName="points"
-          dur="2s"
-          repeatCount="indefinite"
-          values="0,0 512,256 0,512;
-                    20,20 482,226 20,482;
-                    0,0 512,256 0,512"
-          keyTimes="0; 0.5; 1"
-          calcMode="spline"
-          keySplines=".5 0 .5 1; .5 0 .5 1"
-        />
-      </polygon>
-    </svg>
-    <svg
-      v-if="isPlaying"
-      viewBox="0 0 512 512"
-      xmlns="http://www.w3.org/2000/svg"
-      width="50"
-      class="fill-current text-blue-300 hover:text-blue-200"
-    >
-      <polygon points="0,0 0,512 512,512 512,0" />
-    </svg>
-  </button>
+  ></button>
 </template>
 <script>
 export default {
@@ -53,9 +21,38 @@ export default {
 .stop-button {
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%);
+
+  height: 50px;
+
+  border-color: transparent transparent transparent rgb(144, 205, 244);
+  border-width: 25px 0 25px 50px;
+  transition: 200ms border-width ease;
+  border-style: solid;
+}
+
+.play-button:hover,
+.stop-button:hover {
+  border-color: transparent transparent transparent rgb(190, 227, 248);
 }
 .play-button {
-  transform: translate(-35%, -50%);
+  animation: pulse 2s infinite;
+}
+
+.stop-button {
+  height: 40px;
+  border-width: 0 0 0 40px;
+  transform: translate(-50%, -50%);
+}
+
+@keyframes pulse {
+  0% {
+    transform: translate(-35%, -50%) scale(1);
+  }
+  50% {
+    transform: translate(-35%, -50%) scale(1.2);
+  }
+  100% {
+    transform: translate(-35%, -50%) scale(1);
+  }
 }
 </style>
